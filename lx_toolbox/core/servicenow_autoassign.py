00bc7db04327c7a38c8a6d4325f547840c2366f7
Carlos Arias <carias@redhat.com>
2026-03-31 19:48:35 +0200

    Update category in ServiceNowAutoAssign for ticket classification
    - Changed the category from "RHLS Basic External Support" to "RHLS Standard External Support" to better reflect the nature of the support provided.

--- lx_toolbox/core/servicenow_autoassign.py
+++ lx_toolbox/core/servicenow_autoassign.py
@@ -126,7 +126,7 @@ class ServiceNowAutoAssign:
                 self.config.get("T2", "ASSIGNMENT_GROUP_ID")
                 or "974cb3e01bc31c50c57c3224cc4bcbfe"
             ),
-            category="RHLS Basic External Support",
+            category="RHLS Standard External Support",
             subcategory="Course Content",
             issue_type="Other",
             frontend_shift_manager_url=self.config.get("T2", "FRONTEND_OPENSHIFT_ROUTE"),
